ac74e12f1564643f025b8acc7aae868493196cd7
Shinohara Teruki <ts_3156@yahoo.co.jp>
2014-12-17 22:24:35 +0900

    rake db:migrate, adapt posts/new to bootstrap3

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  resources :posts
-
   devise_for :users
 
-  root 'users#index'
   resources :users
+  resources :posts
+
+  root 'posts#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
